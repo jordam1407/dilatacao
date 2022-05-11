bcd83a7c9796e71e63c2810013aa74490e6c923f
jordam mendes <jordammendes@gmail.com>
2022-05-11 08:55:01 -0300

    este

--- App.js
+++ App.js
@@ -1,20 +1,138 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
+import { useState } from 'react'
+import { View, StyleSheet, TextInput, Text, Image, TouchableOpacity, Alert } from "react-native";
 
-export default function App() {
+const Calculaora = () => {
+
+  const [number, onChangeNumber] = React.useState(null);
+  const onPress = () => Alert.alert('APERTOU NÉ DANADO');
+  let [fontsLoaded] = useFonts({
+    'PoppinsMedium': require('./src/fonts/Poppins/PoppinsMedium.ttf'),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.back}>
+      <View style={styles.container}>
+        <Text style={styles.tit} >
+          DILATAÇÃO TÉRMICA
+        </Text>
+        <Image
+          style={{ width: '100%' }}
+          source={require('../dilatacao/images/calc.png')}
+          resizeMode='contain'
+
+
+        />
+        <Text style={styles.parag} >
+          Medida encontrada no diâmetro interno da
+          peça a ser aquecida em mm.
+        </Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="0,00"
+          keyboardType="numeric"
+        />
+        <Text style={styles.parag1} >
+          Diferença entre os diâmetros da peça e do
+          local de montagem em mm.
+        </Text>
+        <TextInput
+          style={styles.input1}
+          onChangeText={onChangeNumber}
+          value={number}
+          placeholder="0,00"
+          keyboardType="numeric"
+
+        />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onPress}
+      >
+        <Text style={styles.calcular}> CALCULAR</Text>
+      </TouchableOpacity>
+
+      </View >
+    </View >
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  back: {
+    backgroundColor: 'rgba(244, 244, 244, 1)',
+    height: '100%',
+    justifyContent: 'center'
   },
+  container: {
+    backgroundColor: 'rgba(244, 244, 244, 1)',
+    marginHorizontal: 35,
+  },
+
+  input: {
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: 'rgba(136, 136, 136, 0.3)',
+    padding: 10,
+    borderRadius: 5,
+    textAlign: 'center',
+    marginBottom: 30,
+    fontSize: 16,
+    fontFamily:'PoppinsMedium',
+  },
+  input1: {
+    backgroundColor: 'white',
+    borderWidth: 1,
+    borderColor: 'rgba(136, 136, 136, 0.3)',
+    padding: 10,
+    borderRadius: 5,
+    textAlign: 'center',
+    marginBottom: 50,
+    fontSize: 16,
+    fontFamily:'PoppinsMedium',
+  },
+  tit: {
+    fontFamily:'PoppinsMedium',
+    marginBottom: 60,
+    fontSize: 24,
+    textAlign: 'center',
+
+  },
+  parag: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginTop: 40,
+    padding: 10,
+    fontFamily:'PoppinsMedium',
+  },
+  parag1: {
+    fontSize: 16,
+    textAlign: 'center',
+    fontFamily:'PoppinsMedium',
+    padding: 10,
+  },
+  calc: {
+    resizeMode: 'contain',
+    width: 120,
+  
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "rgba(24, 144, 255, 1)",
+    padding: 10,
+    borderRadius: 5,
+  },
+  calcular:{
+    fontSize:20,
+        fontFamily:'PoppinsMedium',
+  }
+
+
 });
+
+export default Calculaora;
