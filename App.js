802308ff126432289d9b47e9abb0c12763117953
jordam mendes <jordammendes@gmail.com>
2022-05-11 16:09:41 -0300

    commit

--- App.js
+++ App.js
@@ -2,21 +2,49 @@ import React from "react";
 import { useFonts } from 'expo-font';
 import AppLoading from 'expo-app-loading';
 import { useState } from 'react'
-import { View, StyleSheet, TextInput, Text, Image, TouchableOpacity, Alert } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { View, Modal, StyleSheet, TextInput, Text, Image, TouchableOpacity, Alert } from "react-native";
 
 const Calculaora = () => {
 
-  const [number, onChangeNumber] = React.useState(null);
-  const onPress = () => Alert.alert('APERTOU NÉ DANADO');
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [number, onChangeNumber] = useState('');
+  const [number1, onChangeNumber1] = useState('');
+
+  
   let [fontsLoaded] = useFonts({
     'PoppinsMedium': require('./src/fonts/Poppins/PoppinsMedium.ttf'),
   });
   if (!fontsLoaded) {
     return <AppLoading />;
   }
+  function onModal() {
+    setModalVisible(true)
+  }
+  function onPress() {
+  
+    var aDilatar = Number(number1);
+    var interferencia = Number(number);
+    const folgaMontagem = aDilatar /1000;
+    const alfa = 0.000011454;
+    const deltaT = ((interferencia + folgaMontagem) /aDilatar) / alfa;
+    const ta = 20;
+    const deltaP = 28;
+    var temperatura = Math.round(deltaT + deltaP + ta);
+    if (aDilatar == "" || interferencia == "") {
+        Alert.alert('Insira um número válido');
+    } else {
+      onModal()
+      //Alert.alert('Aquecer a ' + temperatura + 'º'+'.')
+}
+}
+
+
   return (
     <View style={styles.back}>
       <View style={styles.container}>
+
         <Text style={styles.tit} >
           DILATAÇÃO TÉRMICA
         </Text>
@@ -32,32 +60,51 @@ const Calculaora = () => {
           peça a ser aquecida em mm.
         </Text>
         <TextInput
+          
           style={styles.input}
-          onChangeText={onChangeNumber}
-          value={number}
+          onChangeText={onChangeNumber1}
+          value={number1}
           placeholder="0,00"
           keyboardType="numeric"
+          maxLength={7}
         />
         <Text style={styles.parag1} >
           Diferença entre os diâmetros da peça e do
           local de montagem em mm.
         </Text>
         <TextInput
+          step="0.01"
           style={styles.input1}
           onChangeText={onChangeNumber}
           value={number}
           placeholder="0,00"
           keyboardType="numeric"
+          maxLength={4}
 
         />
 
       <TouchableOpacity
         style={styles.button}
         onPress={onPress}
+       //onPress={() => {onPress(); onModal()}}
       >
         <Text style={styles.calcular}> CALCULAR</Text>
       </TouchableOpacity>
-
+      <Modal
+      transparent={true}
+      animationType='fade'
+      visible={modalVisible}
+      onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modal}>
+          <View style={styles.modal1}>
+            <Text>
+              
+            </Text>
+          
+          </View>
+        </View>
+      </Modal>
       </View >
     </View >
   );
@@ -130,6 +177,19 @@ const styles = StyleSheet.create({
   calcular:{
     fontSize:20,
         fontFamily:'PoppinsMedium',
+  },
+  modal:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.2)'
+  },
+  modal1:{
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 35,
+    width: 200,
+    alignItems: 'center'
   }
 
 
